36e0486823216629704563fb378b81d626ad2351
sonu kumar <gautamrocky909621@gmail.com>
2026-09-11 20:22:20 +0530

    removed the dependency of the application.properties from for frontend origins

--- src/main/java/com/sonuSaitring/sonuSaitringManagement/common/config/CorsConfig.java
+++ src/main/java/com/sonuSaitring/sonuSaitringManagement/common/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.frontend.url}")
+    @Value("${FRONTEND_URL}")
     private String frontendUrl;
 
     @Bean
